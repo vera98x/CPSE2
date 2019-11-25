1ed2b12439b0165d5b2930760a043d4dc5b33517
vera98x <31824625+vera98x@users.noreply.github.com>
2019-11-25 11:46:34 +0100

    Update forSS.cpp

--- forSS.cpp
+++ forSS.cpp
@@ -31,3 +31,23 @@ for (int rounds = 0; rounds < 10; rounds++) {
     std::cout << "elapsed time: " << ((end.tv_sec - start.tv_sec) + 1e-9 * (end.tv_nsec - start.tv_nsec)) << "\n";
 }
 
+
+
+    std::cout << "timing for sensorbuffer map: \n";
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    
+    for (int rounds = 0; rounds < 10; rounds++) {
+        for (int i = 0; i < bufferIndexPair; i++) {
+            if (strcmp(bufferPair[i].first, "lat") == 0) {
+                filteredBuffer[filteredBufferIndex] = bufferPair[i];
+                filteredBufferIndex++;
+            }
+        }
+        filteredBufferIndex = 0;
+    }
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    timeresult = ((end.tv_sec - start.tv_sec) + 1e-9 * (end.tv_nsec - start.tv_nsec));
+    std::cout << "elapsed time: " << timeresult << "\n";
+    std::cout << "gem: " << timeresult / 10 << "\n";
+
+
